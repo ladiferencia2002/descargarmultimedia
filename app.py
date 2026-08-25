25e1b7a038bae775fd4895c4e96c683e7efe2dbc
ladiferencia2002 <hugoclaude0612@gmail.com>
2026-08-25 05:23:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -249,5 +249,9 @@ def api_file(job_id):
     return send_file(filepath, as_attachment=True, download_name=job.get("filename"))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
